4915234049693c5b64176df4ea3669824489e496
Matt Sears <mattsears18@gmail.com>
2022-06-25 09:28:39 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import Script from 'next/script';
 import 'odometer/themes/odometer-theme-car.css';
 import { useState, useEffect } from 'react';
 
-const milesTraveled = 714.2;
+const milesTraveled = 912.2;
 
 const Odometer = dynamic(import('react-odometerjs'), {
   ssr: false,
