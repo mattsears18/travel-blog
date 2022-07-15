6690b5f9493cbae55b878272384a7030395bfb23
Matt Sears <mattsears18@gmail.com>
2022-07-14 21:59:41 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faImages } from '@fortawesome/free-solid-svg-icons';
 
-const milesTraveled = 4322.4;
+const milesTraveled = 4749.5;
 
 const Odometer = dynamic(import('react-odometerjs'), {
   ssr: false,
